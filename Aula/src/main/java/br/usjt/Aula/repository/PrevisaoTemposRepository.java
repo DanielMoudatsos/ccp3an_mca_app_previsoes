57da60fd938a9ce1c42f890a9d8edd8c838ee080
Daniel Moudatsos <danielmoudatsosrodrigues@gmail.com>
2019-04-09 19:00:44 -0300

    au0la04

--- Aula/src/main/java/br/usjt/Aula/repository/PrevisaoTemposRepository.java
+++
@@ -1,8 +0,0 @@
-package br.usjt.Aula.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.usjt.Aula.model.PrevisaoTempo;
-
-public interface PrevisaoTemposRepository extends JpaRepository<PrevisaoTempo, Long>{
-
-}
