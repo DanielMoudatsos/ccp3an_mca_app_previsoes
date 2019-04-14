3eef6bd6907ec2436722ea17e16497bf602977e8
Daniel Moudatsos <36973855+DanielMoudatsos@users.noreply.github.com>
2019-04-14 01:24:11 -0300

    Add files via upload

---
+++ Aula01_Refeito/target/classes/import.sql
@@ -0,0 +1,7 @@
+insert into periodo(id, dia_na_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (1, 'Domingo', 12, 16, 54, 'Seco e frio');
+insert into periodo(id, dia_na_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (2, 'Segunda', 11, 15, 73, 'Chuvoso');
+insert into periodo(id, dia_na_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (3, 'Terça', 21, 24, 71, 'Ensolarado');
+insert into periodo(id, dia_na_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (4, 'Quarta', 15, 22, 56, 'Nublado');
+insert into periodo(id, dia_na_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (5, 'Quinta', 14, 19, 62, 'Nublado');
+insert into periodo(id, dia_na_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (6, 'Sexta', 17, 23, 61, 'Nublado');
+insert into periodo(id, dia_na_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (7, 'Sábado', 20, 24, 78, 'Nublado');
